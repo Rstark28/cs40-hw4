568af0debdffeb1d5e24bd334bd01826ef1bbe20
Robert Stark <Robert.Stark@Tufts.edu>
2025-02-21 19:12:52 -0500

    Clean up

--- arith/ppmdiff.c
+++ arith/ppmdiff.c
@@ -1,3 +1,13 @@
+/**************************************************************
+ *
+ *                     ppmdiff.c
+ *
+ *     Assignment: arith
+ *     Authors:    Robert Stark (rstark03), Kyle Wigdor (kwigdo01)
+ *     Date:       2/20/2025
+ *
+ ************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "assert.h"
@@ -7,6 +17,88 @@
 #include "pnm.h"
 #include "a2methods.h"
 #include "a2plain.h"
+
+#define SQUARE(x) ((x) * (x))
+
+static FILE *openFile(char *fname, char *mode);
+void computeDiff(int col, int row, A2Methods_UArray2 array2, A2Methods_Object *ptr, void *cl);
+float computeE(Pnm_ppm image1, Pnm_ppm image2);
+
+/********** main ********
+ *
+ * The main function for the ppmdiff program. It reads two PPM images
+ * from the specified files (or stdin), computes the difference between
+ * them, and prints the result.
+ *
+ * Parameters:
+ *      int argc:       The number of command-line arguments.
+ *      char *argv[]:   An array of command-line arguments.
+ *
+ * Return:
+ *      EXIT_SUCCESS if the program completes successfully,
+ *      EXIT_FAILURE otherwise.
+ *
+ * Expects:
+ *      The program expects at least two command-line arguments specifying
+ *      the input files.
+ *      If "-" is provided as an argument, the program reads from stdin.
+ *      The program can only handle "-" for one of the input files.
+ *
+ * Notes:
+ *      The program will print an error message and exit with EXIT_FAILURE
+ *      if the images have significantly different dimensions or if there
+ *      are issues with reading the files.
+ ************************/
+int main(int argc, char *argv[])
+{
+        if (argc < 3)
+        {
+                return EXIT_FAILURE;
+        }
+
+        FILE *i1_fp;
+        FILE *i2_fp;
+
+        if (strcmp(argv[1], "-") == 0)
+        {
+                i1_fp = stdin;
+        }
+        else
+        {
+                i1_fp = openFile(argv[1], "r");
+        }
+
+        if (strcmp(argv[2], "-") == 0)
+        {
+                if (strcmp(argv[1], "-") == 0)
+                {
+                        fprintf(stderr, "Can't read both inputs from stdin\n");
+                        return EXIT_FAILURE;
+                }
+                else
+                {
+                        i2_fp = stdin;
+                }
+        }
+        else
+        {
+                i2_fp = openFile(argv[2], "r");
+        }
+
+        A2Methods_T methods = uarray2_methods_plain;
+        Pnm_ppm image1 = Pnm_ppmread(i1_fp, methods);
+        Pnm_ppm image2 = Pnm_ppmread(i2_fp, methods);
+
+        if (image1->width > image2->width + 1 ||
+            image2->width > image1->width + 1)
+        {
+                fprintf(stderr, "Image dimensions are too different.\n");
+                return EXIT_FAILURE;
+        }
+
+        printf("%.4f\n", computeE(image1, image2));
+        return EXIT_SUCCESS;
+}
 
 /********** openFile ********
  *
@@ -28,116 +120,130 @@
  ************************/
 static FILE *openFile(char *fname, char *mode)
 {
-    assert(fname != NULL && mode != NULL);
-    FILE *fp = fopen(fname, mode);
-    assert(fp != NULL);
-    return fp;
+        assert(fname != NULL && mode != NULL);
+        FILE *fp = fopen(fname, mode);
+        assert(fp != NULL);
+        return fp;
 }
 
-typedef struct Wrapper {
-    Pnm_ppm other_image;
-    float sum;
-    int self_denom;
+/********** Wrapper ********
+ *
+ * A structure to hold the second image, the sum of differences,
+ * and the denominator of the first image.
+ *
+ * Fields:
+ *      Pnm_ppm other_image: A pointer to the second image.
+ *      float diff:          The sum of squared differences.
+ *      int self_denom:      The denominator of the first image.
+ *
+ * Notes:
+ *      The fields other_image and self_denom must be initialized
+ *      before use. The sum field is used to accumulate the differences.
+ ************************/
+typedef struct Wrapper
+{
+        Pnm_ppm other_image;
+        float diff;
+        int self_denom;
 } Wrapper;
 
-void compute_difference(int col, int row, A2Methods_UArray2 array2,
-                        A2Methods_Object *ptr, void *cl)
+/********** computeDiff ********
+ *
+ * Computes the squared differences between the RGB values of the
+ * corresponding pixels in two images and accumulates the sum.
+ *
+ * Parameters:
+ *      int col:                        The column index of the pixel.
+ *      int row:                        The row index of the pixel.
+ *      A2Methods_UArray2 array2:       The array containing the pixels of the
+ *                                      first image.
+ *      A2Methods_Object *ptr:          A pointer to the current pixel in the
+ *                                      first image.
+ *      void *cl:                       A pointer to a Wrapper structure
+ *                                      containing the second image, the sum of
+ *                                      squared differences, and the
+ *                                      denominator of the first image.
+ *
+ * Return:
+ *      void
+ *
+ * Expects:
+ *      The pointers array2 and cl must not be NULL.
+ *      The pixel coordinates (col, row) must be within the bounds of the
+ *      first image.
+ *
+ * Notes:
+ *      The function will not compute differences for pixels that are out of
+ *      bounds in the second image.
+ ************************/
+void computeDiff(int col,
+                 int row,
+                 A2Methods_UArray2 array2,
+                 A2Methods_Object *ptr, void *cl)
 {
-    assert(cl != NULL);
-    assert(array2 != NULL);
-    Wrapper *w = cl;
+        assert(cl != NULL);
+        assert(array2 != NULL);
+        Wrapper *w = cl;
 
-    Pnm_rgb rgb_this = (void*)ptr; 
+        /* Check if the pixel is out of bounds in the other image */
+        Pnm_ppm other_image = w->other_image;
+        if ((unsigned int)col >= other_image->width ||
+            (unsigned int)row >= other_image->height)
+        {
+                return;
+        }
 
-    if ((col >= w->other_image->methods->width(w->other_image->pixels))
-    || (row >= w->other_image->methods->height(w->other_image->pixels))) {
-        return;
-    }
+        /* Get the RGB values of the current pixel in both images */
+        Pnm_rgb rgb_self = (Pnm_rgb)ptr;
+        Pnm_rgb rgb_other = other_image->methods->at(other_image->pixels, col, row);
+        float self_denom = (float)w->self_denom;
+        float other_denom = (float)other_image->denominator;
 
-    A2Methods_Object *other_ptr = w->other_image->methods->at(w->other_image->pixels, col, row);
-    Pnm_rgb rgb_other = (void*)other_ptr;
+        /* Compute the differences in the RGB values */
+        float r_diff = (rgb_self->red / self_denom) -
+                       (rgb_other->red / other_denom);
+        float g_diff = (rgb_self->green / self_denom) -
+                       (rgb_other->green / other_denom);
+        float b_diff = (rgb_self->blue / self_denom) -
+                       (rgb_other->blue / other_denom);
 
-    float red_diff = ((float)(rgb_this->red) / w->self_denom) - ((float)(rgb_other->red) / w->other_image->denominator);
-    float green_diff = ((float)(rgb_this->green) / w->self_denom) - ((float)(rgb_other->green) / w->other_image->denominator);
-    float blue_diff = ((float)(rgb_this->blue) / w->self_denom) - ((float)(rgb_other->blue) / w->other_image->denominator);
+        float diff = (r_diff * r_diff) +
+                     (g_diff * g_diff) +
+                     (b_diff * b_diff);
 
-    float diff = (red_diff * red_diff) + (green_diff * green_diff) + (blue_diff * blue_diff);
-    
-    w->sum += diff;
+        w->diff += diff;
 }
 
-float computeE(Pnm_ppm image1, Pnm_ppm image2) {
-    assert(image1 != NULL && image2 != NULL);
-    int min_width = image1->width > image2->width ? image2->width : image1->width;
-    int min_height = image1->height > image2->height ? image2->height : image1->height;
-    (void)min_width;
-    (void)min_height;
-
-    Wrapper w = {image2, 0, image1->denominator};
-
-    image1->methods->map_row_major(image1->pixels, compute_difference, &w);
-
-    float denom = 3 * min_width * min_height;
-    return sqrt(w.sum / denom);
-}
-
-int main(int argc, char *argv[])
+/********** computeE ********
+ *
+ * Computes the root mean square error (RMSE) between two PPM images.
+ *
+ * Parameters:
+ *      Pnm_ppm image1: A pointer to the first image.
+ *      Pnm_ppm image2: A pointer to the second image.
+ *
+ * Return:
+ *      float: The computed RMSE value.
+ *
+ * Expects:
+ *      The pointers image1 and image2 must not be NULL.
+ *      The images must have similar dimensions (width and height).
+ *
+ * Notes:
+ *      The function will compute the RMSE based on the overlapping region
+ *      of the two images.
+ ************************/
+float computeE(Pnm_ppm image1, Pnm_ppm image2)
 {
-    (void)argv;
-    A2Methods_T methods = uarray2_methods_plain;
+        assert(image1 != NULL && image2 != NULL);
 
-    (void)methods;
-    if (argc < 3)
-    {
-        return EXIT_FAILURE;
-    }
+        /* Get the minimum width and height of the two images */
+        int min_width = image1->width > image2->width ? image2->width : image1->width;
+        int min_height = image1->height > image2->height ? image2->height : image1->height;
 
-    FILE *i1_fp;
-    FILE *i2_fp;
+        Wrapper w = {image2, 0, image1->denominator};
+        image1->methods->map_row_major(image1->pixels, computeDiff, &w);
+        float denom = 3 * min_width * min_height;
 
-    if (strcmp(argv[1], "-") == 0)
-    {
-        i1_fp = stdin;
-    }
-    else
-    {
-        i1_fp = openFile(argv[1], "r");
-    }
-
-    if (strcmp(argv[2], "-") == 0)
-    {
-        if (strcmp(argv[1], "-") == 0)
-        {
-            fprintf(stderr, "Could not read both inputs from stdin");
-            return EXIT_FAILURE;
-        }
-        else
-        {
-            i2_fp = stdin;
-        }
-    }
-    else
-    {
-        i2_fp = openFile(argv[2], "r");
-    }
-
-    Pnm_ppm image1 = Pnm_ppmread(i1_fp, methods);
-    Pnm_ppm image2 = Pnm_ppmread(i2_fp, methods);
-
-    (void)image1;
-    (void)image2;
-
-    if (((image1->width - image2->width) > 1) ||
-        ((image1->height - image2->height) > 1))
-    {
-        fprintf(stderr, "Could not compare images with too large difference.\n");   
-        printf("1.0");
-        return EXIT_FAILURE;
-    }
-
-    float E = computeE(image1, image2);
-
-    printf("%.4f\n", E);
- 
-    return EXIT_SUCCESS;
+        return sqrt(w.diff / denom);
 }
